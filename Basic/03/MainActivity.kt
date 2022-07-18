aac1903cfbf9a4561b9a3f5d05b64c65d9072b37
SHW98 <sywok19980331@gmail.com>
2022-07-18 23:50:04 +0900

    07.18

--- Basic/03/MainActivity.kt
+++ Basic/03/MainActivity.kt
@@ -1,37 +1,64 @@
 package com.example.test
 
+import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-
 class MainActivity : AppCompatActivity() {
 
-    var onNum:Int = 0
-    var twNum:Int = 0
-    var thNum:Int = 0
-    var foNum:Int = 0
-    var fiNum:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button2.setOnClickListener {
+            val addFunc = {a:Int, b:Int -> a + b}
+            val result = addFunc(10, 10)
 
-        button.setOnClickListener {
-            val on:String = editTextTextPersonName2.text.toString()
-            val tw:String = editTextTextPersonName7.text.toString()
-            val th:String = editTextTextPersonName8.text.toString()
-            val fo:String = editTextTextPersonName9.text.toString()
-            val fi:String = editTextTextPersonName10.text.toString()
-
-            onNum = on.toInt()
-            twNum = tw.toInt()
-            thNum = th.toInt()
-            foNum = fo.toInt()
-            fiNum = fi.toInt()
-
-            var result = onNum + twNum + thNum + foNum + fiNum
-            TextView.setText("결과 : ${result}")
+            textView.setText("더하기 결과 : ${result}")
         }
+
+        button3.setOnClickListener {
+            val addFunc = {a:Int, b:Int -> a + b}
+            val result = calc(30, 10, addFunc)
+
+            textView.setText("더하기 결과 : ${result}")
+        }
+
+        button5.setOnClickListener {
+            val oper = getOperator("subtract")
+            if(oper != null) {
+                val result = calc(30, 10, oper)
+                textView.setText("빼기 결과 : ${result}")
+            }
+        }
+
+        button6.setOnClickListener {
+            val multiply1 = {a:Int, b:Int -> a * b}
+            val multiply2:(Int, Int) -> Int = {a:Int, b:Int -> a * b}
+            val multiply3:(Int, Int) -> Int = {a, b -> a * b}
+
+            val output1 = calc(30, 10, multiply3)
+            textView.setText("곱하기 결과 : ${output1}")
+            
+            val show1 = {println("한 줄 출력")}
+            val show2:()->Unit = {println("한 줄 출력")}
+        }
+    }
+
+    fun calc(first:Int, second:Int, oper:(Int, Int) -> Int):Int {
+        return oper(first, second)
+    }
+
+    fun getOperator(name: String): ((Int,Int) -> Int)? {
+        var oper:((Int, Int)->Int)? = null
+        if (name == "add") {
+            oper = {a, b -> a +b}
+        } else if (name == "subtract") {
+            oper = {a, b -> a - b}
+        }
+
+        return oper
     }
 }
