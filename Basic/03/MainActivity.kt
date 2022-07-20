ba96d24142019b110042aac6cd00d5c92b531a3d
SHW98 <sywok19980331@gmail.com>
2022-07-21 02:06:48 +0900

    07.20

--- Basic/03/MainActivity.kt
+++ Basic/03/MainActivity.kt
@@ -13,26 +13,65 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button2.setOnClickListener {
-            val a = 10
-            val b = 20
-            var max: Int
-
-            if(a>b) {
-                max = a
-            } else {
-                max = b
-            }
-
-            max = if(a > b ) a else b
-
-            max = if(a > b) {
-                textView.setText("a를 선택합니다.")
-                a
-            } else {
-                textView.setText("b를 선택합니다.")
-                b
-            }
-
+            getFirst();
+            getSecond();
+            val result = add(getFirst(), getSecond())
+            show(result)
         }
+
+        button4.setOnClickListener {
+            getFirst();
+            getSecond();
+            val result = sub(getFirst(), getSecond())
+            show(result)
+        }
+
+        button5.setOnClickListener {
+            getFirst();
+            getSecond();
+            val result = mul(getFirst(), getSecond())
+            show(result)
+        }
+
+        button6.setOnClickListener {
+            getFirst();
+            getSecond();
+            val result = div(getFirst(), getSecond())
+            show(result)
+        }
+    }
+
+    fun getFirst():Int {
+        val firstStr = editTextTextPersonName3.text.toString().trim()
+        val first = firstStr.toInt()
+
+        return first
+    }
+
+    fun getSecond():Int {
+        val secondStr = editTextTextPersonName2.text.toString().trim()
+        val second = secondStr.toInt()
+
+        return second
+    }
+
+    fun show(result:Int) {
+        textView.setText("결과 : ${result}")
+    }
+
+    fun add(a:Int, b:Int):Int {
+        return a + b
+    }
+
+    fun sub(a:Int, b:Int):Int {
+        return a - b
+    }
+
+    fun mul(a:Int, b:Int):Int {
+        return a * b
+    }
+
+    fun div(a:Int, b:Int):Int {
+        return a / b
     }
 }
